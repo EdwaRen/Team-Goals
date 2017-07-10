00e79144fda73448673210394d5456037608df7c
Edward Ren <eddie.ren.2013@gmail.com>
2017-07-09 21:50:38 -0400

    Configured initial firebase setup for project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import {firebaseApp} from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
+firebaseApp.auth().onAuthStateChanged(user => {
+
+})
 
 ReactDOM.render(
 
