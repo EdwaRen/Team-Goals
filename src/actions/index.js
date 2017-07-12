262dc196dc3b0ab90430f4959ee1415f62e4500d
Edward Ren <eddie.ren.2013@gmail.com>
2017-07-12 14:03:24 -0400

    Hooked up some redux to the overall project

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import { SIGNED_IN} from '../constants';
+
+export function logUser(email) {
+  const action = {
+    type: SIGNED_IN,
+    email
+
+  }
+
+  return action;
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
+import { connect} from 'react-redux';
+import { firebaseApp} from '../firebase';
+
 
 class App extends Component {
+
+  signOut() {
+    firebaseApp.auth().signOut();
+
+  }
+
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        App
+        <button
+          className = "btn btn-danger"
+          onClick={() => this.signOut()}
+        >
+          Sign Out
+
+        </button>
+
+
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state)
+ {
+   console.log('state', state);
+   return {}
+ }
+export default connect(mapStateToProps, null)(App);
