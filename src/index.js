f0a1c7ef44e6cc31d5595da147c177624ef8497c
Eddie Ren <e2ren@uwaterloo.ca>
2018-01-17 03:46:58 -0500

    Reconfigured support for hashhistory for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore} from 'redux';
 import { Provider} from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, hashHistory } from 'react-router';
 import {firebaseApp} from './firebase';
 import { logUser} from './actions';
 import reducer from './reducers';
@@ -22,17 +22,17 @@ firebaseApp.auth().onAuthStateChanged(user => {
     // console.log('user has signed in or up', user);
     const { email} = user;
     store.dispatch(logUser(email));
-    browserHistory.push('/app');
+    hashHistory.push('/app');
 
   } else {
     // console.log ('user signed out or needs to sign in');
-    browserHistory.replace('/signin');
+    hashHistory.replace('/signin');
   }
 })
 
 ReactDOM.render(
   <Provider store = {store}>
-    <Router path = "/" history = {browserHistory}>
+    <Router path = "/" history = {hashHistory}>
     <Route path = "/app" component = {App} />
     <Route path = "/signin" component = {SignIn} />
     <Route path = "/signup" component = {SignUp} />
